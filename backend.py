fec686009ccd972934d21e0f0271d3582b5a0759
EvgeniiaLukmanova <evgeniialukmanova.gm@gmail.com>
2021-03-31 00:24:38 +0500

    Move uvicorn to routers in backend.py

--- backend.py
+++ backend.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, Body, Request, Form
-from fastapi.responses import JSONResponse, HTMLResponse, RedirectResponse
+import uvicorn
+
+from fastapi import FastAPI, Body, Request
+from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from pydantic import BaseModel, conint, ValidationError
 
-from pathlib import Path
-
 app = FastAPI()
 
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory='.')
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -44,3 +44,6 @@ def __sum(number1: int, number2: int):
 async def pydantic_validation_error_handler(_: Request, exc: ValidationError) -> JSONResponse:
     """ Handles and returns internal exceptions as validation response model"""
     return JSONResponse(exc.errors(), status_code=422)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8080)
